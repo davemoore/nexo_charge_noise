2f441427f5d5c62ad1658c7b8e0ce168e2b1f894
David Moore <david.c.moore@yale.edu>
2020-07-23 14:45:09 -0400

    add some plotting, minor fixes

--- plot_noise.py
+++ plot_noise.py
@@ -5,15 +5,16 @@ import scipy.optimize as opt
 import matplotlib.mlab as mlab
 from matplotlib.backends.backend_pdf import PdfPages
 
+pdf = PdfPages('noise_sim_plots.pdf')
+
 nsamp = 2048
 Fs = 2e6 #Hz
 navg = 700
 nelec = 1.3e5
 nquant = 1.9e5
 
-noise_elec = 200
 
-nmc=1000
+nmc=2000
 
 trace = np.loadtxt("test_trace.txt", delimiter=",", skiprows=1)
 
@@ -28,25 +29,27 @@ tr = tr * nelec/np.sum(tr)
 
 print(np.sum(tr))
 
-b,a = sp.butter(3,0.01)
+b,a = sp.butter(3, 3e5/(Fs/2))
+
+fitpts = [1100,1250]
 
 def ffn(x, A):
-    return A/nelec * tr
+    return A/nelec * tr[fitpts[0]:fitpts[1]]
 
 
 tvec = xvec/Fs
-
-# plt.figure()
-# plt.plot(tvec*1e6,tr)
-# plt.figure()
-# plt.plot(tr)
-# plt.show()
 
 ## noise file from Aldo
 dat = np.loadtxt("Gain_6X_Ready/outnoise_tps.csv", skiprows=2, delimiter=",")
 
 plt.figure()  ## figure showing Aldo's noise spectrum
 plt.loglog( dat[:,0], dat[:,2], lw=2)
+plt.xlabel("Freq [Hz]")
+plt.ylabel("Vnoise [V$^2$/Hz]")
+plt.xlim(1e2, 5e6)
+plt.ylim(1e-14, 1e-10)
+plt.title(r"Noise plot from Aldo, 20191206, $\tau_p = 1.2\mu$s")
+pdf.savefig()
 
 fvec = np.linspace(0, Fs/2, int(nsamp/2)+1)
 camps = np.interp( fvec, dat[:,0], np.sqrt(dat[:,2]) )
@@ -59,7 +62,7 @@ def get_aldo_noise(nlev):
     rvec = camps * rphase
 
     noise_dat = np.fft.irfft(rvec)
-    noise_dat *= nlev/np.std(noise_dat)
+    noise_dat *= (nlev/np.sqrt(2))/np.std(noise_dat[100:-100])  #cut out edge effects in ifft, sqrt(2) samples for ENC
     
     if(False):
         p,f = mlab.psd( noise_dat, Fs = Fs, NFFT=nsamp)
@@ -72,65 +75,113 @@ def get_aldo_noise(nlev):
         plt.show()
         
     return noise_dat
-        
-outvec = []
-outvec2 = []
-for n in range(nmc):
 
-    tf = 1.0*tr
 
-    noise = get_aldo_noise(noise_elec)
-    tf += noise
+noise_rms = [100, 150, 200, 250, 300]
 
-    ##trapezoidal filter
-    outvec.append( np.sum(tf[1186:1226]) )
+noise_type = ["aldo", "white"]
 
-    ## time domain fit
-    bp, bcov = opt.curve_fit(ffn, xvec, tf, p0 = [125000,])
-    outvec2.append( bp[0] )
+white_vec = []
+aldo_vec = []
 
-outvec, outvec2 = np.array(outvec), np.array(outvec2)
+for noise_val in noise_rms:
 
-## now fix the ballistic deficit
-outvec *= nelec/np.median(outvec)
-outvec2 *= nelec/np.median(outvec2)
+    for nt in noise_type:
 
-h, b = np.histogram(outvec)
-h2, b2 = np.histogram(outvec2)
-bc = b[:-1] + np.diff(b)/2
-bc2 = b2[:-1] + np.diff(b2)/2
+        outvec = []
+        outvec2 = []
+        for n in range(nmc):
 
-def gfit(x, A, m, s):
-    return A*np.exp(-(x-m)**2/(2*s**2))
+            tf = 1.0*tr
 
-sig=np.sqrt(h)
-sig[sig==0]=1
-fpts = h>0
-bf, bcov = opt.curve_fit(gfit, bc[fpts], h[fpts], sigma=sig[fpts], p0=[nmc, nelec, 5*noise_elec])
-sig=np.sqrt(h2)
-sig[sig==0]=1
-fpts = h2>0
-bf2, bcov2 = opt.curve_fit(gfit, bc2[fpts], h2[fpts], sigma=sig[fpts], p0=[nmc, nelec, 5*noise_elec])
+            if(nt == "aldo"):
+                noise = get_aldo_noise(noise_val)
+            else:
+                ## AA filter
+                noise = sp.filtfilt(b,a,noise)
+                noise = np.random.randn(nsamp) * noise_val/np.sqrt(2)
 
-xx = np.linspace(nelec-10*noise_elec, nelec+10*noise_elec, int(1e3))
+                
+            tf += noise
+
+            ##trapezoidal filter
+            outvec.append( np.sum(tf[1206:1226]) )
+
+            ## time domain fit
+            bp, bcov = opt.curve_fit(ffn, xvec[fitpts[0]:fitpts[1]], tf[fitpts[0]:fitpts[1]], p0 = [125000,])
+            outvec2.append( bp[0] )
+
+        outvec, outvec2 = np.array(outvec), np.array(outvec2)
+
+        ## now fix the ballistic deficit
+        outvec *= nelec/np.median(outvec)
+        outvec2 *= nelec/np.median(outvec2)
+
+        h, b = np.histogram(outvec)
+        h2, b2 = np.histogram(outvec2)
+        bc = b[:-1] + np.diff(b)/2
+        bc2 = b2[:-1] + np.diff(b2)/2
+
+        def gfit(x, A, m, s):
+            return A*np.exp(-(x-m)**2/(2*s**2))
+
+        sig=np.sqrt(h)
+        sig[sig==0]=1
+        fpts = h>0
+        bf, bcov = opt.curve_fit(gfit, bc[fpts], h[fpts], sigma=sig[fpts], p0=[nmc, np.median(outvec), np.std(outvec)])
+        sig=np.sqrt(h2)
+        sig[sig==0]=1
+        fpts = h2>0
+        bf2, bcov2 = opt.curve_fit(gfit, bc2[fpts], h2[fpts], sigma=sig[fpts], p0=[nmc, np.median(outvec2), np.std(outvec2)])
+
+        plt.figure()
+        plt.errorbar(bc, h, yerr=np.sqrt(h), fmt='k.')
+        plt.errorbar(bc2, h2, yerr=np.sqrt(h2), fmt='r.')
+        xx = np.linspace(bf[1]-3*np.abs(bf[2]), bf[1]+3*np.abs(bf[2]), int(1e3))
+        plt.plot(xx, gfit(xx, *bf), color='k', alpha=0.5, label="Integ. (20 $\mu$s), $\sigma = %d\ e$"%np.abs(bf[2]))
+        xx = np.linspace(bf2[1]-3*np.abs(bf2[2]), bf2[1]+3*np.abs(bf2[2]), int(1e3))
+        plt.plot(xx, gfit(xx, *bf2), color='r', alpha=0.5, label="Fit, $\sigma = %d\ e$"%np.abs(bf2[2]))
+        plt.legend()
+        if(nt == "aldo"):
+            plt.title("ASIC spec, RMS$=%d\ e$, charge noise (fit), $\sigma=%.2f$%%"%(noise_val, 100*np.abs(bf[2])/nquant))
+
+            aldo_vec.append([noise_val, np.abs(bf[2]), np.abs(bf2[2])])
+        else:
+            plt.title("White spec, RMS$=%d\ e$, charge noise (fit), $\sigma=%.2f$%%"%(noise_val, 100*np.abs(bf[2])/nquant))
+            white_vec.append([noise_val, np.abs(bf[2]), np.abs(bf2[2])])
+            
+        pdf.savefig()
+            
+        print(np.mean(outvec), np.std( outvec ))
+
+        print( np.std( outvec )/np.mean( outvec ) )
+        print( np.std( outvec )/nquant )
+
+        print( np.std( outvec2 )/np.mean( outvec2 ) )
+        print( np.std( outvec2 )/nquant )
+
+aldo_vec = np.array(aldo_vec)
+white_vec = np.array(white_vec)
 
 plt.figure()
-plt.errorbar(bc, h, yerr=np.sqrt(h), fmt='k.')
-plt.errorbar(bc2, h2, yerr=np.sqrt(h2), fmt='r.')
-plt.plot(xx, gfit(xx, *bf), color='k', alpha=0.5, label="Integ. (20 $\mu$s), $\sigma = %d\ e$"%bf[2])
-plt.plot(xx, gfit(xx, *bf2), color='r', alpha=0.5, label="Fit, $\sigma = %d\ e$"%bf2[2])
-
+plt.plot( aldo_vec[:,0], aldo_vec[:,1], 'ko-', label="Trap. filt")
+plt.plot( aldo_vec[:,0], aldo_vec[:,2], 'ro-', label='Fit')
+plt.xlabel("Noise RMS [$e$]")
+plt.ylabel("Single channel charge resolution (noise only), [$e$]")
+plt.title("Recon. noise vs charge noise RMS, ASIC spectrum")
 plt.legend()
+pdf.savefig()
 
-print(np.mean(outvec), np.std( outvec ))
+plt.figure()
+plt.plot( white_vec[:,0], white_vec[:,1], 'ko-', label="Trap. filt")
+plt.plot( white_vec[:,0], white_vec[:,2], 'ro-', label='Fit')
+plt.xlabel("Noise RMS [$e$]")
+plt.ylabel("Single channel charge resolution (noise only), [$e$]")
+plt.title("Recon. noise vs charge noise RMS, white spectrum")
+plt.legend()
+pdf.savefig()
 
-print( np.std( outvec )/np.mean( outvec ) )
-print( np.std( outvec )/nquant )
 
-print( np.std( outvec2 )/np.mean( outvec2 ) )
-print( np.std( outvec2 )/nquant )
+pdf.close()
 
 
-
-
-plt.show()
